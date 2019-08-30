0fc1182d512c68b9f945e4c027f9c1a732b8d6d8
ecko <lucasp.1606@gmail.com>
2019-08-30 15:01:42 -0300

    save para no romper

--- cyv/src/app/app-routing.module.ts
+++ cyv/src/app/app-routing.module.ts
@@ -4,26 +4,25 @@ import { SocioComponent } from './socio/socio.component';
 import { TerrenoComponent } from './terreno/terreno.component';
 import { SolicitudTerrenoComponent } from './solicitud-terreno/solicitud-terreno.component';
 import { InicioComponent } from './inicio/inicio.component';
-
-
+import { PrestamoComponent } from './prestamo/prestamo.component';
+import { SolicitudPrestamoComponent } from './solicitud-prestamo/solicitud-prestamo.component';
 
 const routes: Routes = [
   // REDIRECCIONAMIENTO A INICIO
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
 
   // RUTAS A COMPONENTES
-  { path: 'inicio', component: InicioComponent},
-  { path: 'socios', component: SocioComponent},
-  { path: 'terrenos', component: TerrenoComponent},
-  { path: 'solicitud-terreno', component: SolicitudTerrenoComponent},
- // { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
- 
+  { path: 'inicio', component: InicioComponent },
+  { path: 'socios', component: SocioComponent },
+  { path: 'terrenos', component: TerrenoComponent },
+  { path: 'solicitud-terreno', component: SolicitudTerrenoComponent },
+  { path: 'prestamo', component: PrestamoComponent },
+  { path: 'solicitud-prestamo', component: SolicitudPrestamoComponent }
+  // { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
